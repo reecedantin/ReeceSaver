89cbf6a3a5107ba0ea569fbbe7f7259370645887
reecedantin <reecedantin@yahoo.com>
2022-05-04 11:53:34 -0400

    Initial Commit

---
+++ ReeceSaver/ReeceSaverView.h
@@ -0,0 +1,12 @@
+//
+//  ReeceSaverView.h
+//  ReeceSaver
+//
+//  Created by Reece Dantin on 5/4/22.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ReeceSaverView : ScreenSaverView
+
+@end
